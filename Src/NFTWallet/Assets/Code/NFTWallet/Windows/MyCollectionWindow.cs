79f71c1982a4c68f35169796623f4560445fa7b4
noescape00 <NoEscape0@ya.ru>
2022-01-25 20:47:15 +0300

    resize image if it's too big

--- Src/NFTWallet/Assets/Code/NFTWallet/Windows/MyCollectionWindow.cs
+++ Src/NFTWallet/Assets/Code/NFTWallet/Windows/MyCollectionWindow.cs
@@ -142,6 +142,8 @@ public class MyCollectionWindow : WindowBase
 
     private async UniTask<Texture2D> GetRemoteTextureAsync(string url)
     {
+        Texture2D texture; 
+
         using (UnityWebRequest www = UnityWebRequestTexture.GetTexture(url))
         {
             var asyncOp = www.SendWebRequest();
@@ -155,7 +157,29 @@ public class MyCollectionWindow : WindowBase
                 return null;
             }
             
-            return DownloadHandlerTexture.GetContent(www);
+            texture = DownloadHandlerTexture.GetContent(www);
         }
+
+        if (texture != null && (texture.width > 600 || texture.height > 600))
+        {
+            texture = ResizeTexture(texture, 600, 600);
+        }
+
+        return texture;
+    }
+
+    private Texture2D ResizeTexture(Texture2D source, int newWidth, int newHeight)
+    {
+        source.filterMode = FilterMode.Point;
+        RenderTexture rt = RenderTexture.GetTemporary(newWidth, newHeight);
+        rt.filterMode = FilterMode.Point;
+        RenderTexture.active = rt;
+        Graphics.Blit(source, rt);
+        Texture2D nTex = new Texture2D(newWidth, newHeight);
+        nTex.ReadPixels(new Rect(0, 0, newWidth, newHeight), 0, 0);
+        nTex.Apply();
+        RenderTexture.active = null;
+        RenderTexture.ReleaseTemporary(rt);
+        return nTex;
     }
 }
