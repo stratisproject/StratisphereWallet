6378d593d434e271aee9b1c38b52cf73f3b7a22c
xudox <noescape0@ya.ru>
2022-05-25 14:36:14 +0400

    refactored laoding collection window

--- Src/NFTWallet/Assets/Code/NFTWallet/Windows/MyCollectionWindow.cs
+++ Src/NFTWallet/Assets/Code/NFTWallet/Windows/MyCollectionWindow.cs
@@ -46,50 +46,10 @@ public class MyCollectionWindow : WindowBase
 
         await base.ShowAsync(hideOtherWindows);
 
-        List<DeployedNFTModel> knownNfts = NFTWallet.Instance.LoadKnownNfts();
-        OwnedNFTsModel myNfts = await NFTWallet.Instance.StratisUnityManager.Client.GetOwnedNftsAsync(NFTWallet.Instance.StratisUnityManager.GetAddress().ToString());
+        // Populates SpawnedItems
+        await this.LoadCollectionItemsAsync();
 
-        foreach (KeyValuePair<string, ICollection<long>> contrAddrToOwnedIds in myNfts.OwnedIDsByContractAddress)
-        {
-            string contractAddr = contrAddrToOwnedIds.Key;
-            List<long> ownedIds = contrAddrToOwnedIds.Value.Distinct().ToList();
-
-            DeployedNFTModel knownNft = knownNfts.FirstOrDefault(x => x.ContractAddress == contractAddr);
-            string nftName = (knownNft == null)? string.Empty : knownNft.NftName;
-
-            NFTWrapper wrapper = new NFTWrapper(NFTWallet.Instance.StratisUnityManager, contractAddr);
-
-            // Create item for each owned ID or enable already spawned item
-            for (int i = 0; i < ownedIds.Count; i++)
-            {
-                this.StatusText.text = "loading metadata " + contractAddr.Substring(0, 15) + "... " + (i + 1).ToString() + " / " + ownedIds.Count;
-                long currentId = ownedIds[i];
-
-                var alreadySpawnedItem = SpawnedItems.SingleOrDefault(x => x.ContractAddr == contractAddr && x.NFTID == currentId);
-
-                if (alreadySpawnedItem != null)
-                {
-                    alreadySpawnedItem.gameObject.SetActive(true);
-                    continue;
-                }
-
-                CollectionItem cItem = GameObject.Instantiate(CollectionCopyFromItem, ContentGameObject.transform);
-                cItem.gameObject.SetActive(true);
-                SpawnedItems.Add(cItem);
-
-                cItem.NFTID = currentId;
-                cItem.ContractAddr = contractAddr;
-                string uri = await wrapper.TokenURIAsync((UInt256)currentId);
-                cItem.NFTUri = uri;
-
-                cItem.DescriptionText.text = string.Format("{0}  ({1})", nftName, currentId);
-
-                string sellUri = MarketplaceIntegration.Instance.GetSellURI(contractAddr, currentId);
-
-                cItem.Sell_Button.onClick.AddListener(delegate { Application.OpenURL(sellUri); });
-            }
-        }
-
+        // Resize content transform
         RectTransform contentTransform = ContentGameObject.GetComponent<RectTransform>();
         GridLayoutGroup gridLayoutGroup = ContentGameObject.GetComponent<GridLayoutGroup>();
         float cellSize = gridLayoutGroup.cellSize.y;
@@ -103,34 +63,15 @@ public class MyCollectionWindow : WindowBase
         List<UniTask<Texture2D>> loadTasks = new List<UniTask<Texture2D>>();
 
         // Preload json
-        Dictionary<string, string> jsonUriToJson = new Dictionary<string, string>();
-        object locker = new object();
-        List<Task> tasks = new List<Task>();
+        Dictionary<string, string> jsonUriToJson = await this.LoadJsonFilesAsync(notLoaded.Select(x => x.NFTUri).Where(x => x.EndsWith(".json")));
 
-        foreach (string uri in notLoaded.Select(x => x.NFTUri).Where(x => x.EndsWith(".json")))
-        {
-            string u = uri;
-            var task = Task.Run(async () =>
-            {
-                string json = await this.client.GetStringAsync(u);
-
-                lock (locker)
-                {
-                    jsonUriToJson.Add(u, json);
-                }
-            });
-
-            tasks.Add(task);
-        }
-
-        await Task.WhenAll(tasks);
-
+        // Load textures and animations
         List<string> animationsToConvert = new List<string>();
         List<NFTMetadataModel> metadataModels = new List<NFTMetadataModel>();
 
         for (int i = 0; i < notLoaded.Count; i++)
         {
-            this.StatusText.text = "loading textures " + (i + 1).ToString() + " / " + notLoaded.Count;
+            this.StatusText.text = "requesting textures " + (i + 1).ToString() + " / " + notLoaded.Count;
 
             try
             {
@@ -140,31 +81,26 @@ public class MyCollectionWindow : WindowBase
                 bool animationAvailable = false;
                 string animationUrl = null;
 
-                if (uri.EndsWith(".json"))
+                string json = jsonUriToJson[uri];
+                var settings = new JsonSerializerSettings();
+                settings.DateFormatString = "YYYY-MM-DD";
+                settings.ContractResolver = new CustomMetadataResolver();
+                NFTMetadataModel model = JsonConvert.DeserializeObject<NFTMetadataModel>(json, settings);
+
+                metadataModels.Add(model);
+
+                imageUri = model.Image;
+                notLoaded[i].TitleText.text = model.Name;
+
+                string animationUri = (!string.IsNullOrEmpty(model.AnimationUrl)) ? model.AnimationUrl :
+                                        (model.Image.EndsWith(".webp") || model.Image.EndsWith(".gif") || model.Image.EndsWith(".mp4")) ? model.Image : null;
+
+                if (animationUri != null)
                 {
-                    string json = jsonUriToJson[uri];
-                    var settings = new JsonSerializerSettings();
-                    settings.DateFormatString = "YYYY-MM-DD";
-                    settings.ContractResolver = new CustomMetadataResolver();
-                    NFTMetadataModel model = JsonConvert.DeserializeObject<NFTMetadataModel>(json, settings);
-
-                    metadataModels.Add(model);
-
-                    imageUri = model.Image;
-                    notLoaded[i].TitleText.text = model.Name;
-
-                    string animationUri = (!string.IsNullOrEmpty(model.AnimationUrl)) ? model.AnimationUrl :
-                                            (model.Image.EndsWith(".webp") || model.Image.EndsWith(".gif") || model.Image.EndsWith(".mp4")) ? model.Image : null;
-
-                    if (animationUri != null)
-                    {
-                        animationAvailable = true;
-                        animationUrl = animationUri;
-                        animationsToConvert.Add(model.AnimationUrl);
-                    }
+                    animationAvailable = true;
+                    animationUrl = animationUri;
+                    animationsToConvert.Add(model.AnimationUrl);
                 }
-                else
-                    imageUri = uri;
 
                 notLoaded[i].DisplayAnimationButton.gameObject.SetActive(animationAvailable);
 
@@ -178,9 +114,7 @@ public class MyCollectionWindow : WindowBase
                     loadTasks.Add(loadTask);
                 }
                 else
-                {
                     loadTasks.Add(GetNullTextureAsync());
-                }
 
                 if (animationAvailable)
                 {
@@ -205,9 +139,11 @@ public class MyCollectionWindow : WindowBase
         // Convert animations. Fire and forget
         PreconvertAnimations(animationsToConvert);
 
-        this.StatusText.text = "finalizing textures...";
+        this.StatusText.text = "loading textures...";
 
         Texture2D[] loaded = await UniTask.WhenAll(loadTasks);
+
+        this.StatusText.text = "creating sprites...";
 
         for (int i = 0; i < loaded.Length; i++)
         {
@@ -228,7 +164,122 @@ public class MyCollectionWindow : WindowBase
             notLoaded[i].NFTImage.sprite = sprite;
         }
 
-        this.StatusText.text = string.Empty;
+        this.StatusText.text = "Collection loaded";
+    }
+
+    /// <summary>
+    /// Spawns collection items and adds them to SpawnedItems if it's not there.
+    /// Loads URIs.
+    /// </summary>
+    private async Task LoadCollectionItemsAsync()
+    {
+        List<DeployedNFTModel> knownNfts = NFTWallet.Instance.LoadKnownNfts();
+        OwnedNFTsModel myNfts = await NFTWallet.Instance.StratisUnityManager.Client.GetOwnedNftsAsync(NFTWallet.Instance.StratisUnityManager.GetAddress().ToString());
+
+        List<Task> uriLoadTasks = new List<Task>();
+
+        foreach (KeyValuePair<string, ICollection<long>> contrAddrToOwnedIds in myNfts.OwnedIDsByContractAddress)
+        {
+            string contractAddr = contrAddrToOwnedIds.Key;
+            List<long> ownedIds = contrAddrToOwnedIds.Value.Distinct().ToList();
+
+            DeployedNFTModel knownNft = knownNfts.FirstOrDefault(x => x.ContractAddress == contractAddr);
+            string nftName = (knownNft == null) ? string.Empty : knownNft.NftName;
+
+            NFTWrapper wrapper = new NFTWrapper(NFTWallet.Instance.StratisUnityManager, contractAddr);
+
+            // Create item for each owned ID or enable already spawned item
+            for (int i = 0; i < ownedIds.Count; i++)
+            {
+                long currentId = ownedIds[i];
+
+                var alreadySpawnedItem = SpawnedItems.SingleOrDefault(x => x.ContractAddr == contractAddr && x.NFTID == currentId);
+
+                if (alreadySpawnedItem != null)
+                {
+                    alreadySpawnedItem.gameObject.SetActive(true);
+                    continue;
+                }
+
+                CollectionItem cItem = GameObject.Instantiate(CollectionCopyFromItem, ContentGameObject.transform);
+                cItem.gameObject.SetActive(true);
+                SpawnedItems.Add(cItem);
+
+                cItem.NFTID = currentId;
+                cItem.ContractAddr = contractAddr;
+                cItem.DescriptionText.text = string.Format("{0}  ({1})", nftName, currentId);
+                string sellUri = MarketplaceIntegration.Instance.GetSellURI(contractAddr, currentId);
+                cItem.Sell_Button.onClick.AddListener(delegate { Application.OpenURL(sellUri); });
+
+
+                Task TaskUriLoad = Task.Run(async () =>
+                {
+                    string uri = await wrapper.TokenURIAsync((UInt256) currentId);
+                    cItem.NFTUri = uri;
+                });
+
+                uriLoadTasks.Add(TaskUriLoad);
+            }
+        }
+
+        this.StatusText.text = "loading nft URIs...";
+
+        await Task.WhenAll(uriLoadTasks);
+    }
+
+    /// <summary>
+    /// Returns mapping jsonUri to json
+    /// </summary>
+    /// <returns></returns>
+    private async Task<Dictionary<string, string>> LoadJsonFilesAsync(IEnumerable<string> uris)
+    {
+        this.StatusText.text = "loading json metadata";
+
+        Dictionary<string, string> jsonUriToJson = new Dictionary<string, string>();
+        object locker = new object();
+        List<Task> tasks = new List<Task>();
+
+        foreach (string uri in uris)
+        {
+            string u = uri;
+            var task = Task.Run(async () =>
+            {
+                string json = null;
+
+                try
+                {
+                    json = await this.client.GetStringAsync(u);
+                }
+                catch (Exception e)
+                {
+                    Debug.Log(e);
+                }
+
+                lock (locker)
+                {
+                    jsonUriToJson.Add(u, json);
+                }
+            });
+
+            tasks.Add(task);
+        }
+
+        while (true)
+        {
+            await Task.WhenAny(tasks);
+
+            int completedCount = tasks.Count(x => x.IsCompleted);
+
+            await Task.Delay(1);
+            Debug.Log(completedCount);
+            this.StatusText.text = string.Format("loading json metadata {0} / {1}", completedCount, tasks.Count);
+            LayoutRebuilder.ForceRebuildLayoutImmediate(this.StatusText.transform as RectTransform);
+
+            if (completedCount == tasks.Count)
+                break;
+        }
+
+        return jsonUriToJson;
     }
 
     private async UniTask<Texture2D> GetRemoteTextureAsync(string url)
