a55e3a06e6a90a4f06212081643b5033d9dd67a0
xudox <noescape0@ya.ru>
2022-07-10 19:01:33 +0400

    qr code support for logging in

--- Src/NFTWallet/Assets/Code/NFTWallet/Windows/MarketplaceWindow.cs
+++ Src/NFTWallet/Assets/Code/NFTWallet/Windows/MarketplaceWindow.cs
@@ -1,25 +1,114 @@
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.UI;
+using ZXing;
 
 public class MarketplaceWindow : WindowBase
 {
-    public InputField LoginData_InputField, TransferData_InputField;
+    public Button ScanQRButton;
 
-    public Button Login_Button, Transfer_Button;
+    public Text ScanQrButtonText;
 
-    void Awake()
+    public RawImage Image;
+
+    private WebCamTexture webcamTexture;
+
+    private string QrCode = string.Empty;
+
+    private bool isScanning = false;
+
+    async void Awake()
     {
-        this.Login_Button.onClick.AddListener(async delegate
-        {
-            string loginData = LoginData_InputField.text;
-            LoginData_InputField.text = string.Empty;
-            await MarketplaceIntegration.Instance.LogInToNFTMarketplaceAsync(loginData);
-        });
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+            this.Image.transform.Rotate(0, 180, 180);
 
-        this.Transfer_Button.onClick.AddListener(async delegate
+        Image.gameObject.SetActive(false);
+        ScanQrButtonText.text = "Scan QR";
+
+        ScanQRButton.onClick.AddListener(async () =>
         {
-            string transferData = TransferData_InputField.text;
-            TransferData_InputField.text = string.Empty;
-            await MarketplaceIntegration.Instance.ExecuteMarketplaceRequestAsync(transferData);
+            if (!isScanning)
+            {
+                isScanning = true;
+                ScanQrButtonText.text = "Cancel";
+                Debug.Log("SCAN QR PRESSED");
+
+                QrCode = string.Empty;
+                Image.gameObject.SetActive(true);
+                StartCoroutine(GetQRCode());
+            }
+            else
+            {
+                ScanQrButtonText.text = "Scan QR";
+                Image.gameObject.SetActive(false);
+                QrCode = "qwe";
+            }
         });
+    }
+
+    async void Start()
+    {
+        webcamTexture = new WebCamTexture(512, 512);
+        Image.texture = webcamTexture;
+    }
+
+    private IEnumerator GetQRCode()
+    {
+        IBarcodeReader barCodeReader = new BarcodeReader();
+        webcamTexture.Play();
+        Texture2D snap = new Texture2D(webcamTexture.width, webcamTexture.height, TextureFormat.ARGB32, false);
+
+        while (string.IsNullOrEmpty(QrCode))
+        {
+            try
+            {
+                snap.SetPixels32(webcamTexture.GetPixels32());
+                Result result = barCodeReader.Decode(snap.GetRawTextureData(), webcamTexture.width, webcamTexture.height, RGBLuminanceSource.BitmapFormat.ARGB32);
+                if (result != null)
+                {
+                    QrCode = result.Text;
+                    if (!string.IsNullOrEmpty(QrCode))
+                    {
+                        Debug.Log("DECODED TEXT FROM QR: " + QrCode);
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex) { Debug.LogWarning(ex.Message); }
+            yield return null;
+        }
+        webcamTexture.Stop();
+
+        Image.gameObject.SetActive(false);
+        isScanning = false;
+
+
+
+        Task.Run(async () =>
+        {
+            try
+            {
+                await QRCodeScannedAsync(QrCode);
+            }
+            catch (Exception e)
+            {
+                 Debug.LogError(e);
+            }
+        });
+    }
+
+    private async Task QRCodeScannedAsync(string qrCode)
+    {
+        if (qrCode.Contains("login-callback"))
+        {
+            Debug.Log("Logging in");
+            await MarketplaceIntegration.Instance.LogInToNFTMarketplaceAsync(qrCode);
+        }
+        else
+        {
+
+        }
     }
 }
