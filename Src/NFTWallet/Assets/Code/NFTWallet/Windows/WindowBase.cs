5f2f91de2a757de23f879317ac25c031330da7de
Lazar Prijovic @madrazzl3 <madrazzl3@gmail.com>
2022-07-22 20:49:26 +0200

    Fix layout issue for devices with screen cutouts

--- Src/NFTWallet/Assets/Code/NFTWallet/Windows/WindowBase.cs
+++ Src/NFTWallet/Assets/Code/NFTWallet/Windows/WindowBase.cs
@@ -9,10 +9,21 @@ public class WindowBase : MonoBehaviour
             await NFTWalletWindowManager.Instance.HideAllWindowsAsync(this);
 
         this.gameObject.SetActive(true);
+        this.SetParentState(true);
     }
 
     public virtual async UniTask HideAsync()
     {
         this.gameObject.SetActive(false);
+        this.SetParentState(false);
+    }
+
+    private void SetParentState(bool active)
+    {
+        var parent = this.transform.parent.gameObject;
+
+        if (parent != null && parent.GetComponent<WindowParent>() != null) {
+            parent.SetActive(active);
+        }
     }
 }
