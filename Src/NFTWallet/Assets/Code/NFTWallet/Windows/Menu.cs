e4fdca9c82c73d67cf3936c3c30435bfd1cf4629
xudox <noescape0@ya.ru>
2022-08-17 23:37:17 +0400

    ability to delete account

--- Src/NFTWallet/Assets/Code/NFTWallet/Windows/Menu.cs
+++ Src/NFTWallet/Assets/Code/NFTWallet/Windows/Menu.cs
@@ -13,7 +13,18 @@ public class Menu : MonoBehaviour
         //Send_Button.onClick.AddListener(async delegate { await NFTWalletWindowManager.Instance.SendWindow.ShowAsync(); });
         Mint_Button.onClick.AddListener(async delegate { await NFTWalletWindowManager.Instance.MintWindow.ShowAsync(); });
         Burn_Button.onClick.AddListener(async delegate { await NFTWalletWindowManager.Instance.BurnWindow.ShowAsync(); });
-        LogOutButton.onClick.AddListener(async delegate { await NFTWalletWindowManager.Instance.WelcomeWindow.ShowAsync(); });
+        LogOutButton.onClick.AddListener(async delegate
+        {
+            await NFTWalletWindowManager.Instance.PopupWindowYesNo.ShowPopupAsync("Would you like to remove persisted mnemonic from database?", "Delete account?",
+                async () =>
+                {
+                    NFTWallet.Instance.DeleteMnemonic();
+                    await NFTWalletWindowManager.Instance.WelcomeWindow.ShowAsync();
+                }, async () =>
+                {
+                    await NFTWalletWindowManager.Instance.WelcomeWindow.ShowAsync();
+                }, "Yes, delete", "Just logout");
+        });
         MarketplaceButton.onClick.AddListener(async delegate { await NFTWalletWindowManager.Instance.MarketplaceWindow.ShowAsync(); });
     }
 }
--- Src/NFTWallet/Assets/Code/NFTWallet/Windows/PopupWindowYesNo.cs
+++ Src/NFTWallet/Assets/Code/NFTWallet/Windows/PopupWindowYesNo.cs
@@ -6,26 +6,44 @@ using UnityEngine.UI;
 public class PopupWindowYesNo : WindowBase
 {
     public Button CloseButton, ConfirmButton, CancelButton;
-    public Text TitleText, MessageText;
+    public Text TitleText, MessageText, ConfirmButtonText, CancelButtonText;
 
     private Func<Task> onConfirm;
+    private Func<Task> onDeny;
 
     public async void Awake()
     {
-        this.CloseButton.onClick.AddListener(async delegate { await this.HideAsync(); });
-        this.CancelButton.onClick.AddListener(async delegate { await this.HideAsync(); });
         this.ConfirmButton.onClick.AddListener(async delegate
         {
             await this.HideAsync();
-            await this.onConfirm.Invoke();
+            await this.onConfirm?.Invoke();
+        });
+
+        this.CloseButton.onClick.AddListener(async delegate
+        {
+            await this.HideAsync();
+        });
+
+        this.CancelButton.onClick.AddListener(async delegate
+        {
+            await this.HideAsync();
+            await this.onDeny?.Invoke();
         });
     }
 
     public async UniTask ShowPopupAsync(string message, string title, Func<Task> onConfirm)
     {
+        await this.ShowPopupAsync(message, title, onConfirm, null);
+    }
+
+    public async UniTask ShowPopupAsync(string message, string title,Func<Task> onConfirm, Func<Task> onDeny, string confirmButtonText = "Confirm", string denyButtonText = "Cancel")
+    {
         this.MessageText.text = message;
         this.TitleText.text = title;
         this.onConfirm = onConfirm;
+        this.onDeny = onDeny;
+        this.CancelButtonText.text = denyButtonText;
+        this.ConfirmButtonText.text = confirmButtonText;
 
         await this.ShowAsync(false);
     }
